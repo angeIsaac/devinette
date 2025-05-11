8695fdc7bebd8dabcbac91b46a988608a94400a9
Guidy ange isaac <packer02andro69@gmail.com>
2025-05-11 17:00:05 +0000

    ajuot des commentaire

--- src/Devinette.java
+++ src/Devinette.java
@@ -1,3 +1,9 @@
+/**
+ * La classe Devinette encapsule la logique principale d'un jeu
+ * de devinette basé sur la génération d'un nombre aléatoire.
+ * L'objectif du joueur est de deviner ce nombre, avec la possibilité
+ * de recevoir des indices par rapport à ses tentatives.
+ */
 public class Devinette {
     int nombreAleatoire;
 
